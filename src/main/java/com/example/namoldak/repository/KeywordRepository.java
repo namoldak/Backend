db4839bef75f85bcb222aa2429d3e9d9e71dd927
leejincha <leejincha@gmail.com>
2023-01-06 21:00:56 +0900

    [FIX] - 게임시작 API 부분 수정 #37

--- src/main/java/com/example/namoldak/repository/KeywordRepository.java
+++ src/main/java/com/example/namoldak/repository/KeywordRepository.java
@@ -2,6 +2,8 @@ package com.example.namoldak.repository;
 
 import com.example.namoldak.domain.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,11 @@ import java.util.List;
 // 기능 : 게임 카테고리 및 키워드 레포
 @Repository
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
-    List<Keyword> findByCategory(String category);
+    @Query(value = "select * from keyword k where k.category = :category order by rand() limit 4", nativeQuery = true)
+    List<Keyword> findTop4ByCategory(@Param("category") String category);
+
+    @Query(value = "select * from keyword k where k.category = :category order by rand() limit 3", nativeQuery = true)
+    List<Keyword> findTop3ByCategory(@Param("category") String category);
+
 
 }
