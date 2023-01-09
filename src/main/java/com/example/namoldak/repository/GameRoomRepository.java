15552b1c8d76c18783a1737e09ad30d92f071640
PigletHong <hongpiglet1995@gmail.com>
2023-01-09 16:24:06 +0900

    [FIX] 멤버가 방에 들어가있을 경우 방 만들기 기능 제한 #76

--- src/main/java/com/example/namoldak/repository/GameRoomRepository.java
+++ src/main/java/com/example/namoldak/repository/GameRoomRepository.java
@@ -12,5 +12,6 @@ public interface GameRoomRepository extends JpaRepository<GameRoom, Long> {
     Page<GameRoom> findAll(Pageable pageable); // 게임룸 전체 조회 페이징처리
     Page<GameRoom> findByGameRoomNameContaining(Pageable pageable, String keyword); // 게임룸 페이징 처리 + 검색 기능
     Optional<GameRoom> findByGameRoomId(Long gameRoomId); // 게임룸 단건 조회
+    Optional<GameRoom> findByOwner(String owner); // 방장으로 조 단건 조회
 
 }
