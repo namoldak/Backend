837dbce1b8c8f0fd6d14d98a59166f12b163f872
littlezero48 <littlezero.048@gmail.com>
2023-01-11 20:24:46 +0900

    [CHORE] 빠진 파일 추가 #87

---
+++ src/main/java/com/example/namoldak/domain/WebSocketMessage.java
@@ -0,0 +1,18 @@
+package com.example.namoldak.domain;
+
+import lombok.Getter;
+
+// 기능 : 프론트에서 받는 시그널링용 Message
+@Getter
+public class WebSocketMessage {
+    private String sender;
+    private String type;
+    private String data;
+    private Long roomId;
+    private String receiver;
+    private Object offer;
+    private Object answer;
+    private Object candidate;
+    private Object sdp;
+
+}
