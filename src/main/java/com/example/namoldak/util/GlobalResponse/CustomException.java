31dbc10cc792ab65907e6768f8b0af8f85de3a4e
PigletHong <hongpiglet1995@gmail.com>
2023-01-16 14:12:59 +0900

    GameStartSet Redis DB 접속 오류 수정 #120

--- src/main/java/com/example/namoldak/util/GlobalResponse/CustomException.java
+++ src/main/java/com/example/namoldak/util/GlobalResponse/CustomException.java
@@ -6,7 +6,11 @@ import lombok.Getter;
 
 // 기능 : 실행 예외에 ErrorCode필드 추가해 커스텀
 @Getter
-@AllArgsConstructor
 public class CustomException extends RuntimeException {     //실행 예외 클래스를 상속받아서 Unchecked Exception으로 활용
     private final StatusCode statusCode;
+
+    public CustomException(StatusCode statusCode){
+        super(statusCode.getStatusMsg());
+        this.statusCode = statusCode;
+    }
 }
