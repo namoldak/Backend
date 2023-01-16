5bdac4c64c878c7f847b71cd4f306d96c365b657
leejincha <leejincha@gmail.com>
2023-01-16 14:24:44 +0900

    [FIX] 카테고리 랜덤조회 부분 Enum으로 변경  #122

---
+++ src/main/java/com/example/namoldak/domain/Category.java
@@ -0,0 +1,13 @@
+package com.example.namoldak.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum Category {
+
+    인물, 동물, 음식, 만화, 영화, 악기;
+
+    public static Category getRandom(){
+        return values()[(int)(Math.random()* values().length)];
+    }
+}
