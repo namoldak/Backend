8542ec9dd6c82de5296693bf6e62f3c14b605359
isladaisy <isladaisyf@gmail.com>
2023-01-19 02:28:24 +0900

    [BUG] CICD 일단 후퇴 #136

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,22 @@
-#! /usr/bin/env bash
-
-REPOSITORY=/home/ubuntu/app
-cd $REPOSITORY
-
-APP_NAME=namoldak
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-
-CURRENT_PID=$( ps -ef | grep "$JAR_NAME" | grep -v 'grep' | awk '{print $2}')
-
-if [ -z $CURRENT_PID ]
-then
-  echo "> 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
-
-echo"> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH --logging.file.path=$REPOSITORY/log --logging.level.org.hibernate.SQL=DEBUG >> $REPOSITORY/log/deploy.log 2>/$REPOSITORY/log/deploy_err.log &
+##! /usr/bin/env bash
+#
+#REPOSITORY=/home/ubuntu/app
+#cd $REPOSITORY
+#
+#APP_NAME=namoldak
+#JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+#JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+#
+#CURRENT_PID=$( ps -ef | grep "$JAR_NAME" | grep -v 'grep' | awk '{print $2}')
+#
+#if [ -z $CURRENT_PID ]
+#then
+#  echo "> 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+#else
+#  echo "> kill -9 $CURRENT_PID"
+#  kill -15 $CURRENT_PID
+#  sleep 5
+#fi
+#
+#echo"> $JAR_PATH 배포"
+#nohup java -jar $JAR_PATH --logging.file.path=$REPOSITORY/log --logging.level.org.hibernate.SQL=DEBUG >> $REPOSITORY/log/deploy.log 2>/$REPOSITORY/log/deploy_err.log &
