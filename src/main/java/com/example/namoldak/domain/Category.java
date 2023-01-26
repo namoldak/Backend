5c070366a572195548ea482d5b200c6afed60225
PigletHong <hongpiglet1995@gmail.com>
2023-01-26 22:36:55 +0900

    [REFACTOR] 카테고리 수정

--- src/main/java/com/example/namoldak/domain/Category.java
+++ src/main/java/com/example/namoldak/domain/Category.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum Category {
 
-    인물, 동물, 음식, 만화, 영화, 악기;
+    인물, 동물, 음식, 만화, 영화, 드라마;
 
     public static Category getRandom(){
         return values()[(int)(Math.random()* values().length)];
