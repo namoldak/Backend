e3594dbc0399dbdae39a8cf0ee70d452dc2f4fda
PigletHong <hongpiglet1995@gmail.com>
2023-01-31 01:43:52 +0900

    [FEAT] 메세지 초당 입력 횟수 제한 기능 추가 #239

--- src/main/java/com/example/namoldak/service/ChatService.java
+++ src/main/java/com/example/namoldak/service/ChatService.java
@@ -1,6 +1,9 @@
 package com.example.namoldak.service;
 
 import com.example.namoldak.domain.ChatMessage;
+import com.example.namoldak.util.GlobalResponse.CustomException;
+import com.example.namoldak.util.GlobalResponse.code.StatusCode;
+import com.google.common.util.concurrent.RateLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -12,17 +15,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class ChatService{
     private final SimpMessageSendingOperations sendingOperations;
+    private RateLimiter rateLimiter = RateLimiter.create(4.0);
 
-    public void meesage(ChatMessage message){
 
-        ChatMessage exportMessage;
-        exportMessage = ChatMessage.builder()
-                .type(message.getType())
-                .sender(message.getSender())
-                .message(message.getMessage())
-                .build();
-
-        sendingOperations.convertAndSend("/sub/gameRoom/" + message.getRoomId(), exportMessage);
+    public void meesage(ChatMessage message) {
+        if (rateLimiter.tryAcquire()) {
+            ChatMessage exportMessage;
+            exportMessage = ChatMessage.builder()
+                    .type(message.getType())
+                    .sender(message.getSender())
+                    .message(message.getMessage())
+                    .build();
+            sendingOperations.convertAndSend("/sub/gameRoom/" + message.getRoomId(), exportMessage);
+            log.info("===================== 정상 출력");
+        } else {
+            log.info("==================== 그만 쳐라");
+            throw new CustomException(StatusCode.ACCESS_DENIED);
+        }
     }
 
     public void cameraControl(ChatMessage message) {
