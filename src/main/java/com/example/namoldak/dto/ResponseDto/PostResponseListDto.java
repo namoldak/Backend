b50dbbc6ba47e96ced8d3e4eaa252215bfd04e03
PigletHong <hongpiglet1995@gmail.com>
2023-01-31 15:02:24 +0900

    [FIX] 포스트 반환값 원복

--- src/main/java/com/example/namoldak/dto/ResponseDto/PostResponseListDto.java
+++ src/main/java/com/example/namoldak/dto/ResponseDto/PostResponseListDto.java
@@ -8,22 +8,13 @@ import java.util.List;
 public class PostResponseListDto{
     private int totalPage;
     private int postCnt;
-    private boolean myPost;
     private List<PostResponseDto> postResponseDtoList;
-
-    public PostResponseListDto(int totalPage, int postCnt, List<PostResponseDto> postResponseDtoList, boolean myPost) {
-        this.totalPage           = totalPage;
-        this.postCnt             = postCnt;
-        this.myPost            = myPost;
-        this.postResponseDtoList = postResponseDtoList;
-    }
 
     public PostResponseListDto(int totalPage, int postCnt, List<PostResponseDto> postResponseDtoList) {
         this.totalPage           = totalPage;
         this.postCnt             = postCnt;
         this.postResponseDtoList = postResponseDtoList;
     }
-
     public PostResponseListDto(int totalPage, List<PostResponseDto> postResponseDtoList) {
         this.totalPage           = totalPage;
         this.postResponseDtoList = postResponseDtoList;
